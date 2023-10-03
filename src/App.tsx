161daa72392155d3fd0f967956d382dd9b2d46cf
Willian Batista <willian.alves.b15@gmail.com>
2023-10-03 15:05:35 -0300

    criando src e app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Hello Word</Text>
+    </SafeAreaView>
+  );
+}
