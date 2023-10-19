8fe356d2eb8573f957ff28faab5d24672e243319
Willian Batista <willian.alves.b15@gmail.com>
2023-10-19 09:26:49 -0300

    Update carCard.styled.ts

--- src/components/card/carCard.styled.ts
+++ src/components/card/carCard.styled.ts
@@ -1,4 +1,12 @@
 /* prettier-ignore */
 import styled from 'styled-components/native';
 
+export const StyledView = styled.View``;
 
+export const StyledTitle = styled.Text``;
+
+export const StyledText = styled.Text``;
+
+export const ImageCar = styled.Image``;
+
+export const StyledViewText = styled.View``;
