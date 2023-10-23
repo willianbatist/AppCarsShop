af914d2281e1d3017ef5b36e966d70bdc90729b0
Willian Batista <willian.alves.b15@gmail.com>
2023-10-22 22:00:32 -0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,4 @@
 export interface ICar {
   id: number;
-  img: string;
-  brand: string;
-  model: string;
-  description: string;
-  category: string;
-  year: number;
-  price: string;
-  transmission: string;
-  characteristics: string[];
+
 }
